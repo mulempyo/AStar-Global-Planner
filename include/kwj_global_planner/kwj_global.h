686283c1ed37c0bbd09be16117c3ba1a271f195e
mulempyo <luckyp1@naver.com>
2024-09-03 18:35:19 +0900

    success completly

--- include/kwj_global_planner/kwj_global.h
+++ include/kwj_global_planner/kwj_global.h
@@ -13,39 +13,13 @@
 
 namespace astar_planner {
 
-    struct Node {
-        unsigned int x, y;
-        double g_cost, h_cost;
-        Node* parent;
-
-        Node(unsigned int x, unsigned int y, double g, double h, Node* parent = nullptr)
-            : x(x), y(y), g_cost(g), h_cost(h), parent(parent) {}
-
-        double f_cost() const { return g_cost + h_cost; }
-
-        bool operator==(const Node& other) const {
-            return x == other.x && y == other.y;
-        }
-
-        struct HashFunction {
-            std::size_t operator()(const Node& node) const {
-                return std::hash<unsigned int>()(node.x) ^ std::hash<unsigned int>()(node.y);
-            }
-        };
-    };
-
-    struct CompareNodes {
-        bool operator()(const Node* a, const Node* b) const {
-            return a->f_cost() > b->f_cost();
-        }
-    };
-
     class AStarPlanner : public nav_core::BaseGlobalPlanner {
     public:
         AStarPlanner();
         AStarPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
         ~AStarPlanner();
 
+       // Node* createNode(unsigned int x, unsigned int y, double g_cost, double h_cost, Node* parent);
         void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros) override;
         bool makePlan(const geometry_msgs::PoseStamped& start, 
                       const geometry_msgs::PoseStamped& goal, 
@@ -61,14 +35,11 @@ namespace astar_planner {
         unsigned int goal_x_, goal_y_;
 
         boost::mutex mutex_;
-        std::vector<Node*> allocated_nodes_;
-        Node* start_node = nullptr;
-        Node* neighbor = nullptr;
-     
+  
         void publishPlan(const std::vector<geometry_msgs::PoseStamped>& path);
-        std::vector<Node*> aStarSearch(unsigned int start_x, unsigned int start_y, unsigned int goal_x, unsigned int goal_y);
-        std::vector<Node*> getNeighbors(Node* node);
-        std::vector<Node*> reconstructPath(Node* goal_node);
+        std::vector<unsigned int> aStarSearch(unsigned int start_x, unsigned int start_y, unsigned int goal_x, unsigned int goal_y);
+        std::vector<unsigned int> getNeighbors(unsigned int x, unsigned int y);
+        std::vector<unsigned int> reconstructPath(const std::vector<unsigned int>& came_from, unsigned int current_index);
         double potentialFieldCost(unsigned int x, unsigned int y) const;
         double heuristic(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2) const;
         double distance(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2) const;
@@ -77,4 +48,3 @@ namespace astar_planner {
 };
 
 #endif // KWJ_GLOBAL_H
-
